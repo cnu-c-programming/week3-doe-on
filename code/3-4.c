ba4c609e265a0723a81d7b6abf0b84247105839b
doe-on <fovusshi01197@gmail.com>
2026-03-26 09:33:07 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int max_of(int coint, ...) {
+int max_of(int count, ...) {
     va_list a;
     va_start(a, count);
 
@@ -18,9 +18,9 @@ int max_of(int coint, ...) {
 }
 
 int main() {
-    pritnf("%d\n", max_of(4, 5, 12, 3, 9));
-    pritnf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
-    pritnf("%d\n", max_of(3, 100, 200, 300));
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
 
     return 0;
 }
