753e7325c2ab227df59b482ef905c1281275f365
doe-on <fovusshi01197@gmail.com>
2026-03-26 09:14:03 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+void swap(int *x, int*y) {
+    int n = *x;
+    *x = *y;
+    *y = n;
+}
 
+int main() {
+    int x = 100, y =200;
+
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
+    
     return 0;
 }
